6a377a453ff207db115f95e3f13a8ac9dcd91691
Kavya Kunchaparthi <kavyakunchaparthi@gmail.com>
2026-07-25 16:51:41 +0530

    Fix Express backend deployment

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const employeeRoutes = require("./routes/employeeRoutes");
 const assetRoutes = require("./routes/assetRoutes");
+
 const app = express();
 
 app.use(cors());
@@ -15,12 +16,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/employees", employeeRoutes);
 app.use("/api/assets", assetRoutes);
+
 app.get("/", (req, res) => {
     res.send("Inventory & Asset Management Backend is Running...");
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+module.exports = app;
